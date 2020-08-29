f668b9ace25e433281096bd10a9eef3077e43a4c
Jorge <jorgediazok@gmail.com>
2020-08-29 19:28:53 -0300

    created an id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const App = () => {
+  const usersData = [
+    { id: uuidv4(), name: 'Tanya', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Ekaterina', username: 'kateleta' },
+    { id: uuidv4(), name: 'Aleksandr', username: 'sasha25' },
+  ];
+
+  //State
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,11 +21,11 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>View Users</h2>
-          <UserTable />
+          <UserTable users={users} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
